b9876849855c1d3ab58069526a3cd232befc5c97
shamikagangurde <shamika.gangurde@gmail.com>
2016-05-06 09:13:12 -0700

    Added files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+
+Resolutions = new Mongo.Collection('resolutions');
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
+
+
+
+
+
+
+
+
+
